4e66ff36d5b2d1394a4f509308ea6e7984cabf0b
Xyp9xGod <arilson.jlle@gmail.com>
2022-09-15 20:15:03 -0300

    Add authorization to startup

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,10 +1,35 @@
 using GeekShopping.Web.Services;
 using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add Authorization step 1
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+})
+                .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                    options.ClientId = "geek_shopping";
+                    options.ClientSecret = "my_super_secret";
+                    options.ResponseType = "code";
+                    options.ClaimActions.MapJsonKey("role", "role", "role");
+                    options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+                    options.TokenValidationParameters.NameClaimType = "name";
+                    options.TokenValidationParameters.RoleClaimType = "role";
+                    options.Scope.Add("geek_shopping");
+                    options.SaveTokens = true;
+                }
+            );
+
 builder.Services.AddHttpClient<IProductService, ProductService>(c => 
     c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
 );
@@ -21,6 +46,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// Add Authorization step 2
+app.UseAuthentication();
 
 app.UseAuthorization();
 
